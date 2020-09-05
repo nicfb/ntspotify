6e9fb893cafa7d1eedc57fb9dba582337b27a14f
nicfb <bowennicf@gmail.com>
2020-09-05 17:39:42 -0500

    initial working copy

---
+++ public/top.js
@@ -0,0 +1,58 @@
+const api = 'http://localhost:3001';
+
+function RefreshTop() {
+    var timeRange = document.getElementById('timeRange').value;
+    GetTopArtists(timeRange);
+    GetTopTracks(timeRange);
+}
+
+function GetTopArtists(timeRange) {
+    if (timeRange == '') return;
+    fetch(api + '/artists/' + timeRange)
+    .then(resp => resp.json())
+    .then(data => {
+        console.log(data);
+        DisplayTopArtists(data);
+    })
+}
+
+function GetTopTracks(timeRange) {
+    if (timeRange == '') return;
+    fetch(api + '/tracks/' + timeRange)
+    .then(resp => resp.json())
+    .then(data => {
+        console.log(data);
+        DisplayTopTracks(data);
+    })
+}
+
+function DisplayTopArtists(data) {
+    document.getElementById('artistImgs').innerHTML = '';
+    document.getElementById('artistNames').innerHTML = '';
+    data.items.forEach((artist, i) => {
+        var div = document.createElement('div');
+        div.id = 'artist';
+        div.className = 'artist';
+
+        var artistImage = document.createElement('img');
+        artistImage.id = 'imgColumn';
+        artistImage.setAttribute('width', '256')
+        artistImage.setAttribute('height', '256');
+        artistImage.setAttribute('src', artist.images[1].url);
+
+        div.appendChild(artistImage);
+        document.getElementById('artistImgs').appendChild(div);
+    });
+}
+
+function DisplayTopTracks(data) {
+    document.getElementById('trackImgs').innerHTML = '';
+    document.getElementById('trackNames').innerHTML = '';
+    data.items.forEach((track, i) => {
+        var div = document.createElement('div');
+        var trackName = document.createElement('p');
+        trackName.innerHTML = track.name;
+        div.appendChild(trackName);
+        document.getElementById('trackImgs').appendChild(div);
+    });
+}
